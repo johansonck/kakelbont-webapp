b9e46a87238c1ec2802c238b07d129f60b5ad6e1
Johan Sonck <johan.sonck@gmail.com>
2015-12-30 18:44:00 +0100

    wijziging dagprijs en feestdagen/verlof 2016

--- src/main/java/org/kakelbont/webapp/calendar/MainClass.java
+++ src/main/java/org/kakelbont/webapp/calendar/MainClass.java
@@ -1,17 +1,17 @@
 package org.kakelbont.webapp.calendar;
 
+import be.sonck.xml.XmlElement;
+
 import java.util.Calendar;
 import java.util.List;
-
-import be.sonck.xml.XmlElement;
 
 public class MainClass {
 	
 	public static void main(String[] args) throws Exception {
 		CalendarFileReader reader = new CalendarFileReader();
 		
-		List<XmlElement> list = new DefaultCalendarCodeGenerator().generate(2015, Calendar.JANUARY,
-				2015, Calendar.DECEMBER, reader.readFile("/feestdagen.txt"),
+		List<XmlElement> list = new DefaultCalendarCodeGenerator().generate(2016, Calendar.JANUARY,
+				2016, Calendar.DECEMBER, reader.readFile("/feestdagen.txt"),
 				reader.readFile("/verlof.txt"));
 		
 		for (XmlElement element : list) {
